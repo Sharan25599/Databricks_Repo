b8c12abc547a2b7b882ab8fcf6a60d511c6a0e21
Sharan Kumar D <sharankumar.d@diggibyte.com>
2024-03-12 09:06:48 +0530

    Updated employee_silver_to_gold.py

--- Assignment_1/silver_to_gold/employee_silver_to_gold.py
+++ Assignment_1/silver_to_gold/employee_silver_to_gold.py
@@ -12,37 +12,37 @@ employee_df = spark.read.format("delta").load(filepath)
 
 #Find the salary of each department in descending order
 
-employee_df=order_salary_by_department(employee_df, salary_column="salary", department_column="department")
+employee_df1=order_salary_by_department(employee_df, salary_column="salary", department_column="department")
 
 # COMMAND ----------
 
 #Find the number of employees in each department located in each country.
 
-employee_df=group_by_department_country(employee_df,department_column="department",country_column="country")
+employee_df2=group_by_department_country(employee_df,department_column="department",country_column="country")
 
 # COMMAND ----------
 
 #List of the department names along with their corresponding country names. 
 
-result_df = list_department_names(department_df, country_df)
+joined_df = list_department_names(department_df, country_df)
 
 # COMMAND ----------
 
 #The average age of employees in each department
 
-employee_df= avg_age_of_employee(employee_df,department_column="department",age_column="age")
+employee_df3= avg_age_of_employee(employee_df,department_column="department",age_column="age")
 
 # COMMAND ----------
 
 #droped the column load_data
 
-employee_df = drop_columns(employee_df, ["load_date"])
+employee_df4 = drop_columns(employee_df, ["load_date"])
 
 # COMMAND ----------
 
 #added the column at_load_data
 
-employee_df = add_at_load_date(employee_df, new_column_name="at_load_date")
+employee_df5 = add_at_load_date(employee_df, new_column_name="at_load_date")
 
 # COMMAND ----------
 
